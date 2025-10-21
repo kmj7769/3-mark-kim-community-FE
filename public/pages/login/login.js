a80af43e5bbea8d23c3d7a12fd795564d52ba708
kimminjun <rlaalswns7769@gmail.com>
2025-10-21 18:21:23 +0900

    login.js 수정 - 로그인이 허용되지 않는 조건에서 로그인 버튼이 활성화되는 것 방지, 회원가입 경로 리디렉션 절대 경로로 수정

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -25,6 +25,7 @@ document.getElementById("email").addEventListener("change", async function (e) {
   if (email === "") {
     setHelperText("email-help", "이메일을 입력해주세요");
     emailValid = false;
+    checkFormValidity();
     return;
   }
 
@@ -34,6 +35,7 @@ document.getElementById("email").addEventListener("change", async function (e) {
     if (result.status === 400) {
       setHelperText("email-help", "이메일을 입력해주세요");
       emailValid = false;
+      checkFormValidity();
       return;
     }
 
@@ -45,6 +47,7 @@ document.getElementById("email").addEventListener("change", async function (e) {
     } else {
       setHelperText("email-help", "올바른 이메일 주소 형식을 입력해주세요");
       emailValid = false;
+      checkFormValidity();
       return;
     }
   } catch (error) {
@@ -61,6 +64,7 @@ document
     if (password === "") {
       setHelperText("password-help", "비밀번호를 입력해주세요");
       passwordValid = false;
+      checkFormValidity();
       return;
     }
 
@@ -70,6 +74,7 @@ document
       if (result.status === 400) {
         setHelperText("password-help", "비밀번호를 입력해주세요");
         passwordValid = false;
+        checkFormValidity();
         return;
       }
 
@@ -84,6 +89,7 @@ document
           "비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수 문자를 각각 최소 1개 포함해야 합니다"
         );
         passwordValid = false;
+        checkFormValidity();
         return;
       }
     } catch (error) {
@@ -104,7 +110,7 @@ document
       const result = await fetchLogin(email, password);
       if (result.code === 200) {
         console.log("Login successful:", result);
-        window.location.href = "../post-list/post_list.html";
+        window.location.href = "/pages/post-list/post_list.html";
       } else if (result.code === 401) {
         console.log("Login failed:", result);
         setHelperText("password-help", "이메일 또는 비밀번호를 확인해주세요");
