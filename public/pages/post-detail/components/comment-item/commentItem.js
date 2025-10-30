ad98ffcf5ac1ef69c3ff9b4bfe19031cdb4fef62
kimminjun <rlaalswns7769@gmail.com>
2025-10-30 22:50:47 +0900

    게시글 상세 페이지 구현 - 댓글 수정/삭제 버튼이 댓글 호버링 시에만 보이도록 수정

--- public/pages/post-detail/components/comment-item/commentItem.js
+++ public/pages/post-detail/components/comment-item/commentItem.js
@@ -1,30 +1,40 @@
 function commentItemTemplate(comment) {
   return `
-          <li class="comment-item">
-            <img
-              class="comment-profile"
-              src="${
-                comment.profileImage === null ||
-                comment.profileImage === undefined ||
-                comment.profileImage === "null" ||
-                comment.profileImage.trim() === ""
-                  ? "/assets/images/default_profile_img.jpeg"
-                  : comment.profileImage
-              }"
-              alt="프로필 이미지"
-            />
-            <div class="comment-body">
-              <div class="comment-info">
-                <span class="comment-author">${comment.userNickname}</span>
-                <span class="comment-date">${comment.modifiedAt}</span>
-              </div>
-              <div class="comment-content">${comment.content}</div>
-              <div class="comment-action-btns">
-                <button class="comment-edit-btn">수정</button>
-                <button class="comment-delete-btn">삭제</button>
-              </div>
-            </div>
-          </li>
+      <li class="comment-item ${
+        comment.canEdit || comment.canDelete ? "editable-comment" : ""
+      }">
+        <img
+          class="comment-profile"
+          src="${
+            comment.profileImage === null ||
+            comment.profileImage === undefined ||
+            comment.profileImage === "null" ||
+            comment.profileImage.trim() === ""
+              ? "/assets/images/default_profile_img.jpeg"
+              : comment.profileImage
+          }"
+          alt="프로필 이미지"
+        />
+        <div class="comment-body">
+          <div class="comment-info">
+            <span class="comment-author">${comment.userNickname}</span>
+            <span class="comment-date">${comment.modifiedAt}</span>
+          </div>
+          <div class="comment-content">${comment.content}</div>
+        </div>
+        <div class="comment-action-btns">
+          ${
+            comment.canEdit
+              ? '<button class="comment-edit-btn">수정</button>'
+              : ""
+          }
+          ${
+            comment.canDelete
+              ? '<button class="comment-delete-btn">삭제</button>'
+              : ""
+          }
+        </div>
+      </li>
     `;
 }
 
