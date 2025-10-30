290262bad4c7f879cd3cb3236682f09f5fb798a7
kimminjun <rlaalswns7769@gmail.com>
2025-10-30 16:14:32 +0900

    도메인 별 api 통합

--- api/commentApi.js
+++ api/commentApi.js
@@ -1,5 +1,27 @@
 import { backendUrl } from "/config/config.js";
 
+async function fetchAddComment(postId, userId, content) {
+  try {
+    const response = await fetch(backendUrl + `/posts/${postId}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        userId: userId,
+      },
+      body: JSON.stringify({ content: content }),
+    });
+
+    if (response.status !== 201) {
+      throw new Error("Network response was not ok");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching add comment", error);
+    throw error;
+  }
+}
+
 async function fetchCommentList(postId, userId, lastFetchId, limit) {
   try {
     const requestParam =
@@ -28,4 +50,4 @@ async function fetchCommentList(postId, userId, lastFetchId, limit) {
   }
 }
 
-export { fetchCommentList };
+export { fetchCommentList, fetchAddComment };
--- api/postApi.js
+++ api/postApi.js
@@ -23,4 +23,24 @@ async function fetchPostList(lastFetchId, limit) {
   }
 }
 
-export { fetchPostList };
+async function fetchPostDetail(postId, userId) {
+  try {
+    const response = await fetch(backendUrl + `/posts/${postId}`, {
+      method: "GET",
+      headers: {
+        userId: userId,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching post ${postId}`, error);
+    throw error;
+  }
+}
+
+export { fetchPostList, fetchPostDetail };
